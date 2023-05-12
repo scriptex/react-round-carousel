9b401ec405f75df7528b8eda08eca308f6e72a06
Matheus Nasser <matheusnasser12@gmail.com>
2023-05-12 09:41:57 -0300

    added forwardRef + imperativa handle to handle inner methods, and added slideOnClick feature

--- demo/index.tsx
+++ demo/index.tsx
@@ -3,24 +3,28 @@ import { createRoot } from 'react-dom/client';
 
 import 'scriptex-socials';
 
+import { Carousel, CarouselRef } from '../dist/index';
 import { items } from './mocks';
-import { Carousel } from '../dist/index';
 
-const App = () => (
-	<>
-		<a
-			href="https://github.com/scriptex/react-round-carousel"
-			title="See code on Github"
-			className="github-fork-ribbon"
-			data-ribbon="See code on Github"
-		>
-			See code on Github
-		</a>
+const App = () => {
+	const ref = React.createRef<CarouselRef>();
 
-		<Carousel items={items} />
+	return (
+		<>
+			<a
+				href="https://github.com/scriptex/react-round-carousel"
+				title="See code on Github"
+				className="github-fork-ribbon"
+				data-ribbon="See code on Github"
+			>
+				See code on Github
+			</a>
 
-		<social-links></social-links>
-	</>
-);
+			<Carousel items={items} ref={ref} />
+
+			<social-links></social-links>
+		</>
+	);
+};
 
 createRoot(document.getElementById('root')!).render(<App />);
